803bc138dc499568c5a61bd13edba5d27cd04932
Jul Jen <juliajen.prog@gmail.com>
2018-09-04 19:02:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ function deleteFromDriverByKey(driver, key) {
   delete driver_two[key]
   return driver
 }
+
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key]
+    return driver
+}
